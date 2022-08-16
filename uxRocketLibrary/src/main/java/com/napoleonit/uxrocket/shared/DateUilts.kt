5f30976a11bebce8a872b38a0bccb7d3508ec4c4
gleb <g.trubeev@napoleonit.com>
2022-08-16 16:02:10 +0500

    фикс отправки totalValue

--- uxRocketLibrary/src/main/java/com/napoleonit/uxrocket/shared/DateUilts.kt
+++ uxRocketLibrary/src/main/java/com/napoleonit/uxrocket/shared/DateUilts.kt
@@ -4,4 +4,4 @@ import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
 
 const val SERVER_DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS"
-fun getCurrentDateString(): String = DateTime.now(DateTimeZone.UTC).toString(SERVER_DATE_PATTERN)
+fun getCurrentDateString(): String = DateTime.now(DateTimeZone.UTC).toString(SERVER_DATE_PATTERN).replace("-",".")
